ae38b7e9fac0ab45ba802a96b30d091b7358cdcc
umairansari92 <umair.ansari.92@gmail.com>
2025-02-05 14:57:06 +0500

    midified Class 20, 21 code

---
+++ Classes/Module 2 JavaScript/Class 20/app.js
@@ -0,0 +1,86 @@
+// var num1 = 10
+// var num2 = 20
+// var add = num1 + num2
+// console.log(add)
+
+// function addition() {
+//   var num1 = 20;
+//   var num2 = 30;
+//   var add = num1 + num2;
+//   console.log(add);
+// }
+
+// addition()
+
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+// console.log("hello 1")
+
+// addition()
+
+// function add() {
+//   var add = 30 + 20;
+//   console.log(add, "add");
+// }
+
+// function mul() {
+//   var mul = 10 * 9;
+//   console.log("mul", mul);
+// }
+
+// add() // function calling
+// add()
+// add()
+
+// mul()
+// mul()
+
+// function calc() {
+//   var num1 = +prompt("Enter num 1");
+//   var num2 = +prompt("Enter num 2");
+//   var opt = prompt("enter opt ,+-*/");
+
+//   if (opt === "+") {
+//     // console.log("addition", num1 + num2);
+//     document.write("addition" ,  num1 + num2 )
+//   } else if (opt === "-") {
+//     console.log("minus", num1 - num2);
+//   } else if (opt === "*") {
+//     console.log("mul", num1 * num2);
+//   } else if (opt === "/") {
+//     console.log("Div", num1 / num2);
+//   } else {
+//     console.log("invalid");
+//   }
+// }
+
+// function greet(parameters){
+//     alert("Welcome SMIT")
+// }
+
+// greet(args)
+
+// function greet(userName){ //params
+//   // console.log("userName" , userName)
+//   console.log("hello " , userName)
+
+// }
+
+// greet("Jaffar Aman") //args
+
+// greet("bilal")
+
+// function add(num1, num2) {
+//     console.log("num1" , num1)
+//     console.log("num2" , num2)
+//     // console.log("addition", num1 + num2);
+//   }
+  
+//   add(1);
+//   add(100, 200);
--- Classes/Module 2 JavaScript/Class 21/app.js
+++ Classes/Module 2 JavaScript/Class 21/app.js
@@ -1,3 +1,58 @@
 // console.log("Class 21")
 
 
+// console.log("HELLO WORLD!");
+
+// console.log("HELLO 1");
+// function greet() {
+// //   alert("HELLO USER");
+//     console.log("HELLO 3")
+
+// }
+// console.log("HELLO 2");
+
+// greet()
+
+// function add(num1 = 0, num2 = 0, num3 = 0) {
+//   console.log("num1", num1);
+//   console.log("num2", num2);
+
+//   // if(num2 === undefined){
+//   //     num2 = 0
+//   // }
+
+//   console.log("add", num1 + num2 + num3);
+// }
+
+// add(100, 200, 300);
+// add(100, 200);
+// add();
+
+// function greet(userName = "") {
+//   alert("HELLO " + userName);
+// }
+
+// greet("jaffar")
+
+// function foo() {
+//   var add = 50 + 50;
+//   return "add"
+// }
+
+// var temp = foo();
+
+// console.log("temp", temp);
+
+// function addition(num1 = 0, num2 = 0) {
+//     //params
+//     //   console.log(num1 + num2, "add");
+//     //   return num1 + num2; //function sy value lena
+  
+//     return 10;
+  
+//     //   alert("HELLO WORLD");
+//   }
+  
+//   var result = addition(100, 200); //args
+  
+//   console.log("result", result);
