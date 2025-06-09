d3771222c1f4614768ebfd48b83f848709245f1e
umairansari92 <umair.ansari.92@gmail.com>
2025-06-10 01:23:18 +0500

    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/2.App.html #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/app.js #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/index.html #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/quizApp.js #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/report.html #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/report.js #    new file:   Classes/Module 2 JavaScript/Advance JavaScript/Class 02/style.css

---
+++ Classes/Module 2 JavaScript/Advance JavaScript/Class 02/app.js
@@ -0,0 +1,25 @@
+function submitHandler() {
+    const name = document.querySelector("#name").value
+    const email = document.querySelector("#email").value
+
+    console.log(name)
+    console.log(email)
+
+    const userObj = {
+        name,
+        email,
+    }
+
+    localStorage.setItem("user", JSON.stringify(userObj))
+    window.location.replace("./quizApp.html")
+
+
+
+}function getUser() {
+    const user = localStorage.getItem("user")
+    if (user) {
+        const userObj = JSON.parse(user)
+        document.querySelector("#name").value = userObj.name
+        document.querySelector("#email").value = userObj.email
+    }
+}
